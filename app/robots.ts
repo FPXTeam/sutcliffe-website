fa50229c16e1cf6047943f4d292791eaa561f9a0
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-21 16:25:31 +0800

    Make crawler access policy explicit

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,14 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "OAI-SearchBot", allow: "/" },
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
+      { userAgent: "Bingbot", allow: "/" },
+    ],
     sitemap: "https://www.sutcliffetrading.com/sitemap.xml",
     host: "https://www.sutcliffetrading.com",
   };
