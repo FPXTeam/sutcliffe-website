1481f1d3da534c6c36301aee10a56236c51d4dbf
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-21 16:21:33 +0800

    Remove sitewide scroll JavaScript runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import "./site-audit.css";
 import "./brand-system.css";
 import "./motion.css";
 import "./mobile-navigation.css";
-import ScrollExperience from "./components/ScrollExperience";
 import MobileNavigation from "./components/MobileNavigation";
 import AnalyticsConsent, { CookieSettingsButton } from "./components/AnalyticsConsent";
 import "./legal.css";
@@ -73,7 +72,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           <Link className="header-cta" href="/contact">Request timber <span className="header-cta-arrow" aria-hidden="true">→</span></Link>
           <MobileNavigation />
         </header>
-        <ScrollExperience />
         <AnalyticsConsent />
         {children}
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
