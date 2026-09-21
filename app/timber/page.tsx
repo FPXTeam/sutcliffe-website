e2840dfc0f14f61a5c0f96091b74e390d1d5dcbc
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-21 16:36:04 +0800

    Load page styles only on relevant routes

--- app/timber/page.tsx
+++ app/timber/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import ProductCarousel from "../components/ProductCarousel";
+import "../pages.css";
 
 export const metadata: Metadata = {
   title: "Radiata Pine Timber Supply NZ",
